b2c8620c4b329bbaac97fb92be1272436b2f72f5
Geoff <32989059+geoffmcalpin@users.noreply.github.com>
2018-01-20 20:42:42 -0800

    modified quiz-3-5.js & add quiz-3-6.js

---
+++ Lesson_12_Conditionals/quiz-3-5_updated.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ */
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = -5.00;
+var checkBalance = true;
+var isActive = true;
+
+if (checkBalance) {
+    if (!isActive) {
+        console.log("Your account is no longer active.");
+    } if (balance > 0) {
+            console.log("Your balance is $" + balance + ".");
+    } else if (balance === 0) {
+            console.log("Your account is empty.");
+    } else (balance < 0) {
+            console.log("Your balance is negative. Please contact bank.");
+    }
+} else { console.log("Thank you. Have a nice day!");
